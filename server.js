34b9ad87649bd3eeea55b55c0799c394d617d15b
yanpitangui <yanpitangui@gmail.com>
2019-01-13 01:04:22 -0200

    User creation finished

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+const cors = require("cors");
+
+const models = require("./exercise.model");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+	process.env.MONGO_URI,
+	{ useNewUrlParser: true }
+);
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/index.html");
+});
+
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+	let errCode, errMessage;
+
+	if (err.errors) {
+		// mongoose validation error
+		errCode = 400; // bad request
+		const keys = Object.keys(err.errors);
+		// report the first validation error
+		errMessage = err.errors[keys[0]].message;
+	} else {
+		// generic or custom error
+		errCode = err.status || 500;
+		errMessage = err.message || "Internal Server Error";
+	}
+	res
+		.status(errCode)
+		.type("txt")
+		.send(errMessage);
+});
+
+app.post("/api/exercise/new-user", (req, res, next) => {
+	const username = req.body.username;
+	if(username) {
+		let user = new models.User({username});
+		user.save();
+		res.json(user);
+		return next();
+	} else{
+		res.json({"error": "username not found"});
+		return next();
+	}
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+	console.log("Your app is listening on port " + listener.address().port);
+});
